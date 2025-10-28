62d641ca33003525cd66733e54fe7ac5302d3ff7
Jayce <jstephe3@xula.edu>
2025-10-28 18:22:04 -0500

    Add driver.py file that calls amazon.com using requests and prints the title and the first 1000 characters of the HTML

---
+++ driver.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+
+def fetch_page(url, headers=None):
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200: #A response code of 200 indicates success
+        raise Exception(f"Failed to load page {url}, status code: {response.status_code}")
+    
+    return response.text
+
+def main():
+    url = "https://www.amazon.com/"
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/123.0.0.0 Safari/537.36"
+    }
+    
+    page_content = fetch_page(url, headers=headers)
+    soup = BeautifulSoup(page_content, 'html.parser')
+    if(soup is None):
+        print("Failed to parse the page content.")
+        return
+    else:
+        title = soup.title.string
+    
+    print(f"Page Title: {title}")
+    print(soup.prettify()[:1000])
+
+
+if __name__ == "__main__":
+    main()
