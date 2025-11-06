6fc8ad10bf7c921fb084ac63acd85fe92d6be221
Kylan <kylansm98@yahoo.com>
2025-11-05 20:52:07 -0600

    Issue 8 Move is_valid_url function to soup MK

--- driver.py
+++ driver.py
@@ -4,17 +4,7 @@ from urllib.parse import urlparse
 import validators 
 from soupMK import SoupMaker
 
-def is_vaild_url(url):
-    return validators.url(url)
 
-def fetch_page(url, headers=None):
-    if is_vaild_url(url):
-        response = requests.get(url, headers=headers)
-        if response.status_code != 200: #A response code of 200 indicates success
-            raise Exception(f"Failed to load page {url}, status code: {response.status_code}")
-        
-        return response.text
-    raise ValueError(f"Invalid URL format: {url}")
 
 def main():
     url = "https://www.amazon.com/s?k="
